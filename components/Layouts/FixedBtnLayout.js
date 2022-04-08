7e3686b35d805d0d1fb63c682e66471464590ec0
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-04-08 23:24:27 +0100

    z-index

--- components/Layouts/FixedBtnLayout.js
+++ components/Layouts/FixedBtnLayout.js
@@ -5,7 +5,7 @@ const FixedBtnLayout = ({ children, text, action = () => {}, btnColor = "#3CC13B
   return (
     <div className=" max-w-[70rem] mx-auto pb-[1.6rem] relative h-screen flex flex-col">
       {children}
-      <div className=" absolute bottom-[2.4rem] w-full left-1/2 -translate-x-1/2 px-[1.6rem]">
+      <div className=" absolute z-50 bottom-[2.4rem] w-full left-1/2 -translate-x-1/2 px-[1.6rem]">
         <BtnPrimary text={text} color={btnColor}></BtnPrimary>
       </div>
     </div>
--- pages/parties/[id].js
+++ pages/parties/[id].js
@@ -83,7 +83,7 @@ const PartyDetail = () => {
           </Container>
         </section>
 
-        <div className=" fixed bottom-[2.4rem] w-full left-1/2 -translate-x-1/2 px-[1.6rem] max-w-[70rem] mx-auto">
+        <div className=" fixed z-40 bottom-[2.4rem] w-full left-1/2 -translate-x-1/2 px-[1.6rem] max-w-[70rem] mx-auto">
           <BtnPrimary text={"Join Party"} color={"#14B363"}></BtnPrimary>
         </div>
       </BaseLayout>
