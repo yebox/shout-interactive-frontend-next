20d5211fc2c2e8deac5a0bfa149bd9d48496b89a
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-04-20 00:07:45 +0100

    vid-container-cover

--- components/Parties/Video.js
+++ components/Parties/Video.js
@@ -6,7 +6,7 @@ const Video = ({ url }) => {
   const [play, setPlay] = useState(false);
   return (
     <>
-      <div className="relative overflow-hidden rounded-[1.3rem] w-full h-[45vw]  md:h-[20vw]">
+      <div className="relative overflow-hidden rounded-[1.3rem] w-full h-[45vw]  md:h-[20vw] vidCont">
         <div className=" absolute top-0 left-0 w-full flex">
           {/* <Image className=" object-cover flex" height={400} width={700} alt="thumbnail" src={"https://i.ytimg.com/vi/uuZE_IRwLNI/maxresdefault.jpg"}></Image> */}
           {/* <img className="h-full w-full flex" src="/images/cuppy.jpg"></img> */}
